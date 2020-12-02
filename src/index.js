52cc4f9a8e9ffa0ade5bee56db345f7a6d92788e
leo <>
2020-12-02 14:48:43 +0800

    新增项目icon阿里样式表

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'lib-flexible';
 import "@/assets/css/reset.css";
+import "@/assets/font/iconfont.css";
 
 //解决移动端300毫秒延迟
 let FastClick = require('fastclick')
