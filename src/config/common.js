2b9750f1bf4d3fa0550de26781a8e38be0324dda
Jeremy <Jeremyzhu@qq.com>
2021-01-15 16:53:49 +0800

    无用

--- src/config/common.js
+++
@@ -1,6 +0,0 @@
-export function getUrlData(name, str) {
-    const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`);
-    const r = str.substr(1).match(reg);
-    if (r != null) return decodeURIComponent(r[2]);
-    return null;
-}
